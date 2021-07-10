edeb24cf5b5e364101c71f65da486a6d59379003
alejandrodk <ottoabarriosp@hotmail.com>
2021-07-10 10:23:46 -0300

    include theme in app context

--- src/data/AppContext.tsx
+++ src/data/AppContext.tsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
+import {Theme, DarkTheme} from '@styles';
 import { IAppContext } from '@interfaces';
 
 export const AppContext = React.createContext({} as IAppContext);
 
 export const ContextProvider = ({ children }: any) => {
   const [testState, setTestState] = useState(null);
+  const [darkMode, setDarkMode] = useState(false);
 
   const context = {
     testState,
-    setTestState
+    setTestState,
+    setDarkMode,
+    theme: (darkMode ? DarkTheme : Theme)
   };
   
   return <AppContext.Provider value={context}>{children}</AppContext.Provider>;
--- src/lib/interfaces/app.interfaces.tsx
+++ src/lib/interfaces/app.interfaces.tsx
@@ -2,5 +2,42 @@ type ISetState<T> = React.Dispatch<React.SetStateAction<T>>;
 
 export interface IAppContext {
   testState: any;
-  setTestState: ISetState<any>
+  setTestState: ISetState<any>;
+  setDarkMode: ISetState<boolean>;
+  theme: IAppTheme;
+}
+
+export interface IAppTheme {
+  colors: {
+    primary: string;
+    background: string;
+    text_primary: string;
+    text_medium: string;
+    text_soft: string;
+    text_base: string;
+    base: string;
+  };
+  text: {
+    l: any;
+    m: any;
+    s: any;
+  };
+  margin: {
+    l: any;
+    m: any;
+    s: any;
+    xs: any;
+  };
+  padding: {
+    l: any;
+    m: any;
+    s: any;
+    xs: any;
+  };
+  border: {
+    l: any;
+    m: any;
+    s: any;
+    xs: any;
+  };
 }
