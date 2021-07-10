f35670fe2f63e46f80b4c00f0b1a48da2f9d929f
alejandrodk <ottoabarriosp@hotmail.com>
2021-07-09 23:16:10 -0300

    config git hooks with husky

--- src/__tests__/Welcome.test.tsx
+++ src/__tests__/Welcome.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import Welcome from './Welcome';
+import Welcome from '../components/Welcome/Welcome';
 
 test('renders learn react link', () => {
   render(<Welcome />);
